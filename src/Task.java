a3d05c95d071350d9e999551a533818626654596
vmstarchenko <vmstarchenko@edu.hse.ru>
2018-03-24 20:29:53 +0300

    add Task class

---
+++ src/Task.java
@@ -0,0 +1,29 @@
+import java.util.Vector;
+
+
+public class Task {
+    private String description;
+    private Vector<Task> subtasks;
+    private int status = 0; // 0 - todo; 1 - done; 2 - canseled
+
+    public Task(String description) {
+        this.description = description;
+        this.subtasks = new Vector<Task>;
+    }
+
+    void addTask(Task task) {
+        this.subtasks.add(task);
+    }
+
+    void cansel() {
+        status = 2;
+    }
+
+    void done() {
+        status = 1;
+    }
+
+    void todo() {
+        status = 0;
+    }
+}
