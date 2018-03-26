f274bbdbaaa9c2acdec02aeea2869de4edb1a11e
Fexion <fexion1@gmail.com>
2018-03-26 20:04:49 +0300

    editing test objects

--- src/Main.java
+++ src/Main.java
@@ -24,9 +24,11 @@ public class Main {
         Task task1 = new Task("task 1", "some task 1", "28.12.2123");
         Task task2 = new Task("task 2", "some task 2", "18.12.2103");
         Task task4 = new Task("task 4", "some task 4", "28.1.2123");
+
         AutoTask task3 = new AutoTask("task 3", "some task 3", "28.2.2123");
         AutoTask task5 = new AutoTask("task 5", "some task 5", "28.2.2123");
         AutoTask task6 = new AutoTask("task 6", "some task 6", "28.2.2123");
+
         task3.addTask(task5);
         task3.addTask(task6);
 
@@ -35,6 +37,7 @@ public class Main {
         Task subtask3 = new Task("sub_task 3", "some sub_task 3", "28.2.2123");
 
         subtask1.addTask(subtask2);
+
         task1.addTask(subtask1);
         task2.addTask(subtask3);
 
